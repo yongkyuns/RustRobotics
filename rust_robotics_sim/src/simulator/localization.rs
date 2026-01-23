9d75f2c9abf9c93c415c574794f5abdd8125e4ee
Yongkyun Shin <yongkyuns@gmail.com>
2026-01-23 15:44:36 -0500

    feat: use emoji 🗙 for remove button (egui default font)

--- rust_robotics_sim/src/simulator/localization.rs
+++ rust_robotics_sim/src/simulator/localization.rs
@@ -480,7 +480,7 @@ impl Draw for ParticleFilter {
             ui.vertical(|ui| {
                 ui.horizontal(|ui| {
                     ui.label(format!("Vehicle {}", self.id));
-                    if ui.small_button("x").clicked() {
+                    if ui.small_button("🗙").clicked() {
                         keep = false;
                     }
                 });
--- rust_robotics_sim/src/simulator/pendulum.rs
+++ rust_robotics_sim/src/simulator/pendulum.rs
@@ -339,7 +339,7 @@ impl Draw for InvertedPendulum {
                     ui.vertical(|ui| {
                         ui.horizontal(|ui| {
                             ui.label(format!("Pendulum {}", self.id));
-                            if ui.small_button("x").clicked() {
+                            if ui.small_button("🗙").clicked() {
                                 keep = false;
                             }
                         });
